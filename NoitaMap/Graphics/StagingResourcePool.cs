556a79a34ec23993e276b2f9e784ea4f2246f623
chair <projectaa76@gmail.com>
2023-07-19 17:24:58 -0400

    Add basic texture creation test

---
+++ NoitaMap/Graphics/StagingResourcePool.cs
@@ -0,0 +1,183 @@
+﻿using System.Data;
+using Veldrid;
+
+namespace NoitaMap.Graphics;
+
+public class StagingResourcePool : IDisposable
+{
+    private readonly GraphicsDevice GraphicsDevice;
+
+    private readonly List<Texture> Available = new List<Texture>();
+
+    private readonly List<Texture> Used = new List<Texture>();
+
+    private bool Disposed;
+
+    public StagingResourcePool(GraphicsDevice graphicsDevice)
+    {
+        GraphicsDevice = graphicsDevice;
+    }
+
+    public Texture Rent(Texture target)
+    {
+        // Look for any textures that are available and match the target
+        for (int i = 0; i < Available.Count; i++)
+        {
+            Texture texture = Available[i];
+
+            if (target.Type == texture.Type && target.Format == texture.Format)
+            {
+                switch (target.Type)
+                {
+                    case TextureType.Texture1D:
+                        if (target.Width == texture.Width)
+                        {
+                            Available.RemoveAt(i--);
+                            Used.Add(texture);
+
+                            return texture;
+                        }
+                        break;
+                    case TextureType.Texture2D:
+                        if (target.Width == texture.Width && target.Height == texture.Height)
+                        {
+                            Available.RemoveAt(i--);
+                            Used.Add(texture);
+
+                            return texture;
+                        }
+                        break;
+                    case TextureType.Texture3D:
+                        if (target.Width == texture.Width && target.Height == texture.Height && target.Depth == texture.Depth)
+                        {
+                            Available.RemoveAt(i--);
+                            Used.Add(texture);
+
+                            return texture;
+                        }
+                        break;
+
+                }
+            }
+        }
+
+        // Create a new texture based on the target
+        Texture pooled = GraphicsDevice.ResourceFactory.CreateTexture(new TextureDescription()
+        {
+            Width = target.Width,
+            Height = target.Height,
+            Depth = target.Depth,
+            Format = target.Format,
+            Type = target.Type,
+            ArrayLayers = target.ArrayLayers,
+            MipLevels = 1,
+            Usage = TextureUsage.Staging,
+            SampleCount = TextureSampleCount.Count1
+        });
+
+        Used.Add(pooled);
+
+        return pooled;
+    }
+
+    public Texture Rent(TextureDescription target)
+    {
+        // Look for any textures that are available and match the target
+        for (int i = 0; i < Available.Count; i++)
+        {
+            Texture texture = Available[i];
+
+            if (target.Type == texture.Type && target.Format == texture.Format)
+            {
+                switch (target.Type)
+                {
+                    case TextureType.Texture1D:
+                        if (target.Width == texture.Width)
+                        {
+                            Available.RemoveAt(i--);
+                            Used.Add(texture);
+
+                            return texture;
+                        }
+                        break;
+                    case TextureType.Texture2D:
+                        if (target.Width == texture.Width && target.Height == texture.Height)
+                        {
+                            Available.RemoveAt(i--);
+                            Used.Add(texture);
+
+                            return texture;
+                        }
+                        break;
+                    case TextureType.Texture3D:
+                        if (target.Width == texture.Width && target.Height == texture.Height && target.Depth == texture.Depth)
+                        {
+                            Available.RemoveAt(i--);
+                            Used.Add(texture);
+
+                            return texture;
+                        }
+                        break;
+
+                }
+            }
+        }
+
+        // Create a new texture based on the target
+        Texture pooled = GraphicsDevice.ResourceFactory.CreateTexture(new TextureDescription()
+        {
+            Width = target.Width,
+            Height = target.Height,
+            Depth = target.Depth,
+            Format = target.Format,
+            Type = target.Type,
+            ArrayLayers = target.ArrayLayers,
+            MipLevels = 1,
+            Usage = TextureUsage.Staging,
+            SampleCount = TextureSampleCount.Count1
+        });
+
+        Used.Add(pooled);
+
+        return pooled;
+    }
+
+    public void Return(Texture resource)
+    {
+        if (!Used.Contains(resource))
+        {
+            throw new ArgumentException("Pool does not own that resource", nameof(resource));
+        }
+
+        Used.Remove(resource);
+        Available.Add(resource);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!Disposed)
+        {
+            foreach (Texture texture in Available)
+            {
+                texture.Dispose();
+            }
+
+            Available.Clear();
+
+            foreach (Texture texture in Used)
+            {
+                texture.Dispose();
+            }
+
+            Used.Clear();
+
+            Disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
--- NoitaMap/Viewer/ViewerDisplay.cs
+++ NoitaMap/Viewer/ViewerDisplay.cs
@@ -1,9 +1,13 @@
-﻿using CommunityToolkit.HighPerformance.Helpers;
+﻿using System.Runtime.InteropServices;
+using CommunityToolkit.HighPerformance.Helpers;
+using NoitaMap.Graphics;
+using NoitaMap.Map;
 using Silk.NET.GLFW;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
 using Silk.NET.Windowing.Extensions.Veldrid;
 using Veldrid;
+using Vulkan;
 
 namespace NoitaMap.Viewer;
 
@@ -16,6 +20,12 @@ public class ViewerDisplay : IDisposable
     private readonly CommandList MainCommandList;
 
     private Framebuffer MainFrameBuffer;
+
+    private readonly StagingResourcePool StagingResourcePool;
+
+    private readonly Texture TestTexture;
+
+    private readonly MaterialProvider MaterialProvider;
 
     private bool Disposed;
 
@@ -42,6 +52,31 @@ public class ViewerDisplay : IDisposable
         MainCommandList = GraphicsDevice.ResourceFactory.CreateCommandList();
 
         MainFrameBuffer = GraphicsDevice.MainSwapchain.Framebuffer;
+
+        StagingResourcePool = new StagingResourcePool(GraphicsDevice);
+
+        MaterialProvider = new MaterialProvider();
+
+        Material brick = MaterialProvider.GetMaterial("brick");
+
+        TextureDescription desc = new TextureDescription()
+        {
+            Type = TextureType.Texture2D,
+            Format = PixelFormat.R8_G8_B8_A8_UNorm,
+            Width = (uint)brick.MaterialTexture.Width,
+            Height = (uint)brick.MaterialTexture.Height,
+            Usage = TextureUsage.Staging,
+            MipLevels = 1,
+
+            // Nececessary
+            Depth = 1,
+            ArrayLayers = 1,
+            SampleCount = TextureSampleCount.Count1,
+        };
+
+        TestTexture = GraphicsDevice.ResourceFactory.CreateTexture(desc);
+
+        GraphicsDevice.UpdateTexture(TestTexture, MemoryMarshal.CreateSpan(ref brick.MaterialTexture.Span.DangerousGetReference(), (int)brick.MaterialTexture.Length), 0, 0, 0, (uint)brick.MaterialTexture.Width, (uint)brick.MaterialTexture.Height, 1, 0, 0);
 
         Window.Center();
 
