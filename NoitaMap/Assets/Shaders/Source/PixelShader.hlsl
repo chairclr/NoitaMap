001156d35c50b6052fa74052739a2643d6452a34
chair <projectaa76@gmail.com>
2023-07-20 17:43:46 -0400

    Fix sampler name in `PixelShader.hlsl`

--- NoitaMap/Assets/Shaders/Source/PixelShader.hlsl
+++ NoitaMap/Assets/Shaders/Source/PixelShader.hlsl
@@ -1,13 +1,13 @@
 #include "Common.hlsl"
 
-SamplerState LinearSamplerView : SAMPLER : register(s0);
+SamplerState PointSamplerView : SAMPLER : register(s0);
 Texture2D MainTextureView : TEXTURE : register(t0);
 
 PS_OUTPUT PSMain(PS_INPUT input) : SV_TARGET
 {
     PS_OUTPUT output;
     
-    float4 textureColor = MainTextureView.Sample(LinearSamplerView, input.uv);
+    float4 textureColor = MainTextureView.Sample(PointSamplerView, input.uv);
     
     float4 finalColor = textureColor;
     
